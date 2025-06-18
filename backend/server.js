03e13dbceb12c70100a61b913fc56e04fea25d34
Raghulkannan005 <raghulkannan005@gmail.com>
2025-06-18 20:29:01 +0530

    BASE APP

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({
+    origin : process.env.FRONTEND_URL
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , ()=>{
+    console.log("Server is running on port", PORT);
+})
+
+export default app;
