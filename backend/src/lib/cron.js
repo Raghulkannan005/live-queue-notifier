fd9a9935b512e7703b2aa55801c01ff8a1aed2be
Raghulkannan005 <raghulkannan005@gmail.com>
2025-06-26 14:44:41 +0530

    Fixed : Cron running every second

--- backend/src/lib/cron.js
+++ backend/src/lib/cron.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const job = new CronJob('* */14 * * * *', async () => {
+const job = new CronJob('0 */14 * * * *', async () => {
   const url = `${process.env.API_URL}/test`;
 
   try {
